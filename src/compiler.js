205db4db4dfe55beb5e9d2b040c0800ad674b217
Stephen Crane <scrane@mozilla.com>
2012-06-28 15:09:20 -0700

    Restructured pass interface (added initialize function).

--- src/compiler.js
+++ src/compiler.js
@@ -1490,6 +1490,9 @@
   function compile(node, name, _logger, options) {
     // The logger is closed over by all the functions.
     logger = _logger;
+    var o = { name: name, logger: _logger, warn: warningOptions(options), jsInput: options['js-input'], memcheck: options.memcheck };
+
+    jsFrontend.initialize(o);
 
     // Lift into constructors.
     node = T.lift(node);
@@ -1498,8 +1501,9 @@
 
     logger.info("Pass 1");
     var types = resolveAndLintTypes(node, clone(Types.builtinTypes));
-    var o = { types: types, name: name, logger: _logger, warn: warningOptions(options), jsInput: options['js-input'], memcheck: options.memcheck };
+    o.types = types;
 
+    // Pass 1.5 - lifting vars into frame level lets
     if (options['js-input']) {
       node.jsRewrite(o);
     }
@@ -1508,7 +1512,10 @@
     logger.info("Pass 2");
     node.scan(o);
 
-    node.externPass(o);
+    // Pass 2.5 - include externs for all undeclared global vars
+    if (options['js-input']) {
+      node.externPass(o);
+    }
 
     // Pass 3.
     logger.info("Pass 3");
